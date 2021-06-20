bd92b767f3c686f25ce72188e4c3e2bf8c72437c
Walker <walker@praiseweb.com.br>
2021-06-20 16:30:01 -0300

    feat: add config dir

--- run.sh
+++ run.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 set -e
+DEF_CONFIG_DIR=$(realpath ~/.config)/nubank_sync_ynab
+CFG_DIR=${CONFIG_DIR:-$DEF_CONFIG_DIR}
+VOLUME=$CFG_DIR:/tmp/nubank_sync_ynab
+
 if [[ $1 == "generate" ]]; then
-    docker-compose run generate
+    docker-compose run -v $VOLUME generate
 else
     DEF_STARTING_POINT=$(date -d "$(systemctl show nubank-sync-ynab.timer --property=ActiveEnterTimestamp |
     awk -F= '{print $2}')" +%Y-%m-%d)
     STARTING_POINT=${1:-$DEF_STARTING_POINT}
     echo "last run was $STARTING_POINT"
-    NUBANK_CERT=$(cat /tmp/nubank_sync_ynab/cert)
-    NUBANK_TOKEN=$(cat /tmp/nubank_sync_ynab/token)
-    NUBANK_CARD_ACCOUNT=$(cat /tmp/nubank_sync_ynab/nubank_card_account)
-    NUBANK_NUCONTA_ACCOUNT=$(cat /tmp/nubank_sync_ynab/nubank_nuconta_account)
-    YNAB_EMAIL=$(cat /tmp/nubank_sync_ynab/ynab_email)
-    YNAB_PASSWORD=$(cat /tmp/nubank_sync_ynab/ynab_pass)
-    YNAB_BUDGET=$(cat /tmp/nubank_sync_ynab/ymab_budget)
+    NUBANK_CERT=$(cat $CFG_DIR/cert)
+    NUBANK_TOKEN=$(cat $CFG_DIR/token)
+    NUBANK_CARD_ACCOUNT=$(cat $CFG_DIR/nubank_card_account)
+    NUBANK_NUCONTA_ACCOUNT=$(cat $CFG_DIR/nubank_nuconta_account)
+    YNAB_EMAIL=$(cat $CFG_DIR/ynab_email)
+    YNAB_PASSWORD=$(cat $CFG_DIR/ynab_pass)
+    YNAB_BUDGET=$(cat $CFG_DIR/ymab_budget)
     docker-compose run \
+        -v $VOLUME
         -e STARTING_POINT=$STARTING_POINT \
         -e NUBANK_CERT=$NUBANK_CERT \
         -e NUBANK_TOKEN=$NUBANK_TOKEN \
